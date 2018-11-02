c4e947bf324441f852d6056c4f2417e3d0b48adc
BRBrewster <186741@student.asd20.org>
2018-11-02 14:49:11 -0600

    no message

---
+++ Archive  11-2-18/B.Brewster TicTacToe/B.Brewster TicTacToe/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace B.Brewster_TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] board = new int[3, 3];
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j] = i * j;
+                }
+
+            }
+            printBoard(board);
+            Console.WriteLine("What row do you want?:");
+            string[] y = Console.ReadLine();
+        }
+        static void printBoard(int[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] > 9)
+                    {
+                        Console.Write(board[i, j] + " | ");
+                    }
+                    else
+                    {
+                        Console.Write(" " + board[i, j] + " | ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
