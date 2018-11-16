f67ae4192396b12b9182aca58dd49653587bcd0c
BRBrewster <186741@student.asd20.org>
2018-11-16 14:51:45 -0700

    asd

--- BBrewster Character save load/BBrewster Character save load/Character.cs
+++ BBrewster Character save load/BBrewster Character save load/Character.cs
@@ -6,5 +6,18 @@ namespace BBrewster_Character_save_load
 {
     class Character
     {
+        string name;
+        int intelligence;
+        int strength;
+        int luck;
+        int health;
+        public Character(string name,int intelligence, int strength, int luck, int health)
+        {
+            this.name = name;
+            this.intelligence = intelligence;
+            this.strength = strength;
+            this.luck = luck;
+            this.health = health;
+        }
     }
 }
