a8b53dafaa5933a5b2f3a8b2f48ed10d5e61cf23
BRBrewster <186741@student.asd20.org>
2018-11-27 14:49:44 -0700

    Character save/Load save file fixed

--- BBrewster Character save load/BBrewster Character save load/Character.cs
+++ BBrewster Character save load/BBrewster Character save load/Character.cs
@@ -6,11 +6,11 @@ namespace BBrewster_Character_save_load
 {
     class Character
     {
-        string name;
-        int intelligence;
-        int strength;
-        int luck;
-        int health;
+        public string name;
+        public int intelligence;
+        public int strength;
+        public int luck;
+        public int health;
         public Character(string name,int intelligence, int strength, int luck, int health)
         {
             this.name = name;
--- BBrewster Character save load/BBrewster Character save load/Program.cs
+++ BBrewster Character save load/BBrewster Character save load/Program.cs
@@ -37,7 +37,15 @@ namespace BBrewster_Character_save_load
                 }
                 if (answer == 4)
                 {
-
+                    Console.Write("What is your character's name?: ");
+                    string characterName = Console.ReadLine();
+                    LoadCharacter(characterName);
+                    Console.WriteLine("Welcome back" + characterName + " your stats are:");
+                    Console.WriteLine(newCharacter.name);
+                    Console.WriteLine(newCharacter.intelligence);
+                    Console.WriteLine(newCharacter.strength);
+                    Console.WriteLine(newCharacter.health);
+                    Console.WriteLine(newCharacter.luck);
                 }
                 if (answer == 5)
                 {
@@ -56,11 +64,12 @@ namespace BBrewster_Character_save_load
             int s = Convert.ToInt32(Console.ReadLine());
             Console.Write("What is your luck?: ");
             int l = Convert.ToInt32(Console.ReadLine());
-            Character newcharacter = new Character(n, i, s, h, l);
+            Character newCharacter = new Character(n, i, s, h, l);
             Console.Write("Sucess! " + n + ". Your health is 100");
             Console.WriteLine();
-            SaveCharacter(newcharacter);
-            return newcharacter;
+            SaveCharacter(newCharacter);
+            return newCharacter;
+            
         }
         static void ModifyCharacter(Character character)
         {
@@ -69,16 +78,64 @@ namespace BBrewster_Character_save_load
         static void SaveCharacter(Character newCharacter)
         {
             string path = @"C:\Users\186741\Documents\Junior-year-coding-class\BBrewster Character saveload\BBrewster Character saveload\bin\Debug\netcoreapp2.1\Character.txt";
-            path = AppDomain.CurrentDomain.BaseDirectory + @"Character.txt";
+            path = AppDomain.CurrentDomain.BaseDirectory + @"Character_" + newCharacter.name +".txt";
             List<Character> characters = new List<Character>();
             characters.Add(newCharacter);
             using (StreamWriter sw = new StreamWriter(path))
             {
-                sw.WriteLine(newCharacter);
+                sw.WriteLine(newCharacter.name);
+                sw.WriteLine(newCharacter.intelligence);
+                sw.WriteLine(newCharacter.strength);
+                sw.WriteLine(newCharacter.health);
+                sw.WriteLine(newCharacter.luck);
             }
-            
-
         }
+        static Character LoadCharacter(string characterName)
+        {
+            string n = " ";
+            int i = 0;
+            int s = 0;
+            int l= 0;
+            int h= 0;
+            string path = @"C:\Users\186741\Documents\Junior-year-coding-class\BBrewster Character saveload\BBrewster Character saveload\bin\Debug\netcoreapp2.1\Character.txt";
+            path = AppDomain.CurrentDomain.BaseDirectory + @"Character_" + characterName + ".txt";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for(int j=0; j<6; j++)
+                {
+                    if (j == 1)
+                    {
+                        n = Console.ReadLine();
+                    }
+                    else if (j == 2)
+                    {
+                        i = Convert.ToInt32(Console.ReadLine());
+                    }
+                    else if (j == 3)
+                    {
+                        s = Convert.ToInt32(Console.ReadLine());
+                    }
+                    else if (j == 4)
+                    {
+                        h = 100;
+                    }
+                    else if (j == 5)
+                    {
+                        l = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+            }
+            Character newCharacter = new Character(n, i, s, h, l);
+            return newCharacter;
+        }
+        //List<Character> LoadCharacters()
+        //{
+            //using (StreamReader sr = new StreamReader(AppDomain.CurrentDomain.BaseDirectory))
+            //{
+                //namespace.Contains(".txt")
+            //}
+            
+        //}
         //bool DeleteCharacter(List<Character> characters, string n)
         //{
 
