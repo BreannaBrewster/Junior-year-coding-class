f812621702e17cfe2391cf784e08b1f4b9da5183
BRBrewster <186741@student.asd20.org>
2018-12-03 14:00:26 -0700

    Change sprites

---
+++ Purple Bunny Maze/Assets/Script/ChangeSprite.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSprite : MonoBehaviour {
+
+    public Sprite Left, Right, Up, Down;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.GetComponent<SpriteRenderer>().sprite = Right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.GetComponent<SpriteRenderer>().sprite = Left;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.GetComponent<SpriteRenderer>().sprite = Up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.GetComponent<SpriteRenderer>().sprite = Down;
+        }
+    }
+}
