16572452943e0b6ec7310d1c13a69dea39504b02
BRBrewster <186741@student.asd20.org>
2019-01-28 13:12:26 -0700

    Queue program completed

---
+++ Semester 2/Queue/Queue/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu = 1;
+            Queue myQueue = new Queue();
+            while (menu != 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("****************************************************");
+                Console.WriteLine("press 1 to add a number to the back of the queue");
+                Console.WriteLine("press 2 to remove the front element of the queue");
+                Console.WriteLine("press 3 to return the front element of the queue");
+                Console.WriteLine("press 4 to print the list of element in the queue");
+                Console.WriteLine("press 5 to find the position of a number in the queue");
+                Console.WriteLine("press 0 to exit");
+                menu = int.Parse(Console.ReadLine());
+                if (menu == 1)
+                {
+                    Console.Write("What number would you like to add to the queue?:");
+                    int a = int.Parse(Console.ReadLine());
+                    myQueue.EnQueue(a);
+                }
+                if (menu == 2)
+                {
+                    myQueue.DeQueue();
+                }
+                if (menu == 3)
+                {
+                    myQueue.peek();
+                }
+                if (menu == 4)
+                {
+                    myQueue.print();
+                }
+                if (menu == 5)
+                {
+                    Console.Write("What number would you like to search for?: ");
+                    int a = int.Parse(Console.ReadLine());
+                    myQueue.Search(a);
+                }
+            }
+        }
+    }
+}
---
+++ Semester 2/Queue/Queue/Queue.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue
+{
+    class Queue
+    {
+        int b;
+        List<int> hold = new List<int>();
+        public void stack()
+        {
+
+        }
+        public void EnQueue(int a)
+        {
+            hold.Add(a);
+        }
+        public int DeQueue()
+        {
+
+            if (hold.Count < 2)
+            {
+                Console.WriteLine("There is" + hold.Count + " element in this stack. Are you sure you want to delete this stack? (Y/N)");
+                string x = Console.ReadLine().ToUpper();
+                if (x[0] == 'Y' || x[0] == 'y')
+                {
+                    List<int> hold = new List<int>();
+                }
+            }
+            else
+            {
+                b = hold[hold[0]];
+                hold.RemoveAt(hold[0]);
+                Console.WriteLine(b);
+                for(int i=1;i<hold.Count;i++)
+                {
+                    hold[i - 1] = hold[i];
+                }
+            }
+            return b;
+        }
+        public int peek()
+        {
+            if (hold.Count < 1)
+            {
+                Console.WriteLine("This queue is empty");
+            }
+            else
+            {
+                Console.Write(hold[hold.Count - 1]);
+                Console.WriteLine();
+            }
+            return b;
+        }
+        public void print()
+        {
+            foreach (int i in hold)
+            {
+                Console.Write(i + ",");
+            }
+            Console.WriteLine();
+        }
+        public int Search(int a)
+        {
+            int c = -1;
+            int d = 0;
+            Console.WriteLine("The value" + a + "is located at the position(s): ");
+            foreach (int i in hold)
+            {
+                if (d > 1)
+                {
+                    Console.Write(", ");
+                }
+                c++;
+                if (i == a)
+                {
+                    d++;
+                    Console.Write(c);
+
+                }
+            }
+            return c;
+        }
+    }
+}
+
