e21d3eea44a2fba468e862f9a4f8bdbbb6b7b032
BRBrewster <186741@student.asd20.org>
2019-04-08 14:49:52 -0600

    no message

--- Semester 2/Unity/Final/Assets/Script/Death.cs
+++ Semester 2/Unity/Final/Assets/Script/Death.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Death : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    float deathTime;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        deathTime += Time.deltaTime;
+        if (deathTime > 5)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
--- Semester 2/Unity/Final/Assets/Script/Enemy.cs
+++ Semester 2/Unity/Final/Assets/Script/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
     public Vector3 velocity = new Vector3(0, 0, 20);
     int speed = 10;
-    float deathTime;
+
     public Rigidbody rb;
     Random rand = new Random();
     // Use this for initialization
@@ -17,10 +17,6 @@ public class Enemy : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.position += new Vector3(Random.Range(-5,6),0,-5) * Time.deltaTime * speed;
-        deathTime += Time.deltaTime;
-        if(deathTime >5)
-        {
-            Destroy(gameObject);
-        }
+        
     }
 }
