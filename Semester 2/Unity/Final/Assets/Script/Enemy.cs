a820178f0f1d0f084b20cb6bc998b8a92978e3e6
BRBrewster <186741@student.asd20.org>
2019-04-08 14:49:16 -0600

    Final Game project created

---
+++ Semester 2/Unity/Final/Assets/Script/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public Vector3 velocity = new Vector3(0, 0, 20);
+    int speed = 10;
+    float deathTime;
+    public Rigidbody rb;
+    Random rand = new Random();
+    // Use this for initialization
+    void Start () {
+        velocity = new Vector3(0, 0, -5);
+        velocity.Normalize();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(Random.Range(-5,6),0,-5) * Time.deltaTime * speed;
+        deathTime += Time.deltaTime;
+        if(deathTime >5)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Semester 2/Unity/Final/Assets/Script/EnemyManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+    public float spawnTime = 2;
+    public GameObject Enemy;
+    //public List<Vector3> spawnPoints = new List<Vector3>();
+    [SerializeField]
+    float speed = 2f;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnTime += spawnTime * .8f;
+    }
+    void Spawn()
+    {
+        GameObject newEnemy = Instantiate(Enemy);
+        newEnemy.transform.position = new Vector3(0, 1.2f, 50.76f);
+        newEnemy.GetComponent<Enemy>().velocity = new Vector3(-0.2f, 0, 0);
+    }
+
+}
